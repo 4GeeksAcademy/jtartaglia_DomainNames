4255e5c075ecda71a29d8d74fffeface201bf876
julitar <126813769+julitar@users.noreply.github.com>
2023-04-20 17:45:39 +0000

    Primera versión JS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+/* window.onload = function() { */
+
+let pronoun = ["my", "your"];
+let adjective = ["wacky", "goofy", "silly"];
+let noun = ["code", "home", "market", "webpage"];
+let extension = [".com", ".es"];
+let domainNames = "";
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adjective.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        domainNames += `${pronoun[i]}${adjective[j]}${noun[k]}${extension[l]}\n`;
+      }
+    }
+  }
+}
+console.log(domainNames);
